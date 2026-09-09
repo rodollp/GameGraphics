54098318e8ac4d5fc4dea88799d5eecfdcb6fd04
rodollp <zeus02021101@gmail.com>
2026-09-09 17:50:23 +0900

    R

--- GameGraphics/Assets/GameGraphics/Script/Day10/ClickEffectSpawner.cs
+++ GameGraphics/Assets/GameGraphics/Script/Day10/ClickEffectSpawner.cs
@@ -6,33 +6,86 @@ namespace Day10
     public class ClickEffectSpawner : MonoBehaviour
     {
         [SerializeField] private Camera targetCamera;
-        [SerializeField] private ParticleSystem effectPrefab;
         [SerializeField] private LayerMask groundMask;
-
         [SerializeField] private EffectPool effectPool;
+
+        [Header("Hold Settings")]
+        [SerializeField] private float holdDelay = 0.2f;
+        [SerializeField] private float spawnInterval = 0.1f;
 
         private Vector2 pointerPosition;
 
-        public void OnPoint(InputValue value)
+        private bool isPressed;
+        private bool isHolding;
+
+        private float holdTimer;
+        private float spawnTimer;
+
+        public void OnPoint(InputAction.CallbackContext context)
         {
-            pointerPosition = value.Get<Vector2>();
+            pointerPosition = context.ReadValue<Vector2>();
         }
 
-        public void OnClick(InputValue value)
+        public void OnClick(InputAction.CallbackContext context)
         {
-            if (!value.isPressed)
+            if (context.performed)
             {
+                isPressed = true;
+                isHolding = false;
+
+                holdTimer = 0f;
+                spawnTimer = 0f;
+
+                SpawnEffect();
+            }
+
+            if (context.canceled)
+            {
+                isPressed = false;
+                isHolding = false;
+
+                holdTimer = 0f;
+                spawnTimer = 0f;
+            }
+        }
+
+        private void Update()
+        {
+            if (!isPressed)
+                return;
+
+            holdTimer += Time.deltaTime;
+
+            // 아직 꾹 누르기로 판단하지 않음 
+            if (!isHolding)
+            {
+                if (holdTimer >= holdDelay)
+                {
+                    isHolding = true;
+                    spawnTimer = 0f;
+                }
+
                 return;
             }
 
+            // 꾹 누르는 중 
+            spawnTimer += Time.deltaTime;
+
+            if (spawnTimer >= spawnInterval)
+            {
+                spawnTimer = 0f;
+                SpawnEffect();
+            }
+        }
+
+        private void SpawnEffect()
+        {
             Ray ray = targetCamera.ScreenPointToRay(pointerPosition);
 
             if (Physics.Raycast(ray, out RaycastHit hit, 100f, groundMask))
             {
                 effectPool.PlayEffect(hit.point);
-                
-                
             }
         }
     }
-}
+} 
